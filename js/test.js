25b33604985dfcc83444a7523271749aa7664187
legoshu01 <eru.legoshu01@gmail.com>
2025-12-06 13:10:58 +0900

    更新

--- js/test.js
+++ js/test.js
@@ -1,47 +1,20 @@
 function initMap() {
   const map = new google.maps.Map(document.getElementById("map"), {
     zoom: 5,
-    center: { lat: 35.681236, lng: 139.767125 }, // 東京駅中心
+    center: { lat: 43.0, lng: 142.0 }, // 北海道の中心
   });
 
-  // 北海道の GeoJSON（国土地理院の境界データを簡略化したもの）
-  const hokkaidoGeoJson = {
-    "type": "Feature",
-    "properties": { "name": "Hokkaido" },
-    "geometry": {
-      "type": "Polygon",
-      "coordinates": [[
-        [141.6, 45.6], 
-        [145.4, 44.5],
-        [146.2, 43.3],
-        [143.2, 41.7],
-        [139.8, 41.2],
-        [139.0, 43.1],
-        [141.6, 45.6]
-      ]]
-    }
-  };
+  fetch('./hokkaido.geojson')
+    .then(res => res.json())
+    .then(geojson => {
+      console.log("読み込んだ GeoJSON:", geojson);
+      map.data.addGeoJson(geojson);
 
-  // 地図に読み込む
-  map.data.addGeoJson(hokkaidoGeoJson);
-
-  // 通常のスタイル
-  map.data.setStyle({
-    fillColor: "transparent",
-    strokeColor: "#555",
-    strokeWeight: 2
-  });
-
-  // ホバー時に赤く
-  map.data.addListener("mouseover", function (event) {
-    map.data.overrideStyle(event.feature, {
-      strokeColor: "red",
-      strokeWeight: 4
-    });
-  });
-
-  // ホバー解除で元に戻す
-  map.data.addListener("mouseout", function (event) {
-    map.data.revertStyle();
-  });
+      map.data.setStyle({
+        fillColor: "transparent",
+        strokeColor: "#f00",
+        strokeWeight: 2
+      });
+    })
+    .catch(err => console.error("エラー:", err));
 }
